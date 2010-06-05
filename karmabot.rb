3f9587ac6202a6beb51e9ea7313fde30b6024e3d
shura <shura1991@gmail.com>
2010-06-05 15:08:11 +0200

    KarmaBot++

--- karmabot.rb
+++ karmabot.rb
@@ -1,149 +1,149 @@
-require 'socket'
-require 'openssl'
-require 'timeout'
-require 'sqlite3'
-require 'thread'
-require 'net/ping'
-require 'getopt/std'
+class KarmaBot
+    require 'socket'
+    require 'openssl'
+    require 'timeout'
+    require 'sqlite3'
+    require 'thread'
+    require 'net/ping'
+    require 'getopt/std'
 
-class Socket
-    def initialize(server, port, ssl = false)
-        @sock = timeout(30, STDERR){
-            TCPSocket.open(server, port)
-        }
-        @use_ssl = ssl
-        if @use_ssl
-            ssl = OpenSSL::SSL::SSLSocket.new(@sock)
-            ssl.connect
-            @s = ssl
-        else
-            @s = @sock
-        end
-    end
-
-    def eof?
-        @s.eof?
-    end
-
-    def puts(str)
-        if str !~ /^NAMES #/
-            $stdout.puts "<< " + str
-        end
-        @s.puts(str)
-    end
-
-    def gets
-        @s.gets
-    end
-
-    def close
-        if @use_ssl
-            @sock.close
-        else
-            @s.close
-        end
-    end
-end
-
-class Karma
-    def initialize(dbname = 'karma.db')
-        @db = SQLite3::Database.new(dbname)
-        @db.execute("CREATE TABLE IF NOT EXISTS karma (
-            nick    VARCHAR(50) NOT NULL PRIMARY KEY,
-            karma   INT NOT NULL DEFAULT 0
-        );")
-    end
-
-    def increment(nick)
-        begin
-            @db.execute("INSERT INTO karma (nick) VALUES ('#{nick}')")
-        rescue SQLite3::SQLException
-        end
-        @db.execute("UPDATE karma SET karma = karma + 1 WHERE nick = '#{nick}'")
-    end
-
-    def decrement(nick)
-        begin
-            @db.execute("INSERT INTO karma (nick) VALUES ('#{nick}')")
-        rescue SQLite3::SQLException
-        end
-        @db.execute("UPDATE karma SET karma = karma - 1 WHERE nick = '#{nick}'")
-    end
-
-    def getKarma(nick)
-        @db.get_first_value("SELECT karma FROM karma WHERE nick = '#{nick}';").to_i
-    end
-
-    def close
-        @db.close
-    end
-end
-
-class Names
-    def initialize(chan)
-        if chan !~ /^#[\\`\{\}\[\]\-_A-Z0-9\|\^]+$/i
-            raise "Erroneous channel name"
-        end
-        @chan = chan
-        @names = []
-        @parsing = false
-    end
-
-    def parseMessage(message)
-        message.scan(/^:.+? 353 .+? = #{@chan} :(.+?)\s*$/){ |nicks|
-            if not @parsing
-                @names = nicks[0].gsub(/[+%@&~]/, '').split(/ /)
-                @parsing = true
+    class Socket
+        def initialize(server, port, ssl = false)
+            @sock = timeout(30, STDERR){
+                TCPSocket.open(server, port)
+            }
+            @use_ssl = ssl
+            if @use_ssl
+                @s = OpenSSL::SSL::SSLSocket.new(@sock)
+                @s.connect
             else
-                @names += nicks[0].gsub(/[+%@&~]/, '').split(/ /)
+                @s = @sock
             end
-        }
+        end
+    
+        def eof?
+            @s.eof?
+        end
+    
+        def puts(str)
+            if str !~ /^NAMES #/
+                $stdout.puts "<< " + str
+            end
+            @s.puts(str)
+        end
+    
+        def gets
+            @s.gets
+        end
+    
+        def close
+            if @use_ssl
+                @sock.close
+            else
+                @s.close
+            end
+        end
+    end
 
-        if message =~ /^:.+? 366 .+? #{@chan} :End of \/NAMES list.\s*$/
+    class Karma
+        def initialize(dbname = 'karma.db')
+            @db = SQLite3::Database.new(dbname)
+            @db.execute("CREATE TABLE IF NOT EXISTS karma (
+                nick    VARCHAR(50) NOT NULL PRIMARY KEY,
+                karma   INT NOT NULL DEFAULT 0
+            );")
+        end
+    
+        def increment(nick)
+            begin
+                @db.execute("INSERT INTO karma (nick) VALUES ('#{nick}')")
+            rescue SQLite3::SQLException
+            end
+            @db.execute("UPDATE karma SET karma = karma + 1 WHERE nick = '#{nick}'")
+        end
+    
+        def decrement(nick)
+            begin
+                @db.execute("INSERT INTO karma (nick) VALUES ('#{nick}')")
+            rescue SQLite3::SQLException
+            end
+            @db.execute("UPDATE karma SET karma = karma - 1 WHERE nick = '#{nick}'")
+        end
+    
+        def getKarma(nick)
+            @db.get_first_value("SELECT karma FROM karma WHERE nick = '#{nick}';").to_i
+        end
+    
+        def close
+            @db.close
+        end
+    end
+
+
+    class Names
+        def initialize(chan)
+            if chan !~ /^#[\\`\{\}\[\]\-_A-Z0-9\|\^]+$/i
+                raise "Erroneous channel name"
+            end
+            @chan = chan
+            @names = []
             @parsing = false
         end
+    
+        def parseMessage(message)
+            message.scan(/^:.+? 353 .+? = #{@chan} :(.+?)\s*$/){ |nicks|
+                if not @parsing
+                    @names = nicks[0].gsub(/[+%@&~]/, '').split(/ /)
+                    @parsing = true
+                else
+                    @names += nicks[0].gsub(/[+%@&~]/, '').split(/ /)
+                end
+            }
+    
+            if message =~ /^:.+? 366 .+? #{@chan} :End of \/NAMES list.\s*$/
+                @parsing = false
+            end
+    
+            message.scan(/^:([\\`\{\}\[\]\-_A-Z0-9\|\^]+)!.+?@.+? JOIN :#{@chan}\s+$/i){ |nick|
+                @names += nick
+            }
+            
+            message.scan(/^:[\\`\{\}\[\]\-_A-Z0-9\|\^]+!.+?@.+? KICK #{@chan} ([\\`\{\}\[\]\-_A-Z0-9\|\^]+) :/i){ |nick|
+                @names -= nick
+            }
+    
+            message.scan(/^:([\\`\{\}\[\]\-_A-Z0-9\|\^]+)!.+?@.+? PART #{@chan}/i){ |nick|
+                @names -= nick
+            }
+    
+            message.scan(/^:([\\`\{\}\[\]\-_A-Z0-9\|\^]+)!.+?@.+? QUIT/i){ |nick|
+                @names -= nick
+            }
 
-        message.scan(/^:([\\`\{\}\[\]\-_A-Z0-9\|\^]+)!.+?@.+? JOIN :#{@chan}\s+$/i){ |nick|
-            @names += nick
-        }
-        
-        message.scan(/^:[\\`\{\}\[\]\-_A-Z0-9\|\^]+!.+?@.+? KICK #{@chan} ([\\`\{\}\[\]\-_A-Z0-9\|\^]+) :/i){ |nick|
-            @names -= nick
-        }
+            message.scan(/^:([\\`\{\}\[\]\-_A-Z0-9\|\^]+)!.+?@.+? NICK :([\\`\{\}\[\]\-_A-Z0-9\|\^]+)/i){ |pnick, nick|
+                @names -= [pnick]
+                @names += [nick]
+            }
+        end
 
-        message.scan(/^:([\\`\{\}\[\]\-_A-Z0-9\|\^]+)!.+?@.+? PART #{@chan}/i){ |nick|
-            @names -= nick
-        }
-
-        message.scan(/^:([\\`\{\}\[\]\-_A-Z0-9\|\^]+)!.+?@.+? QUIT/i){ |nick|
-            @names -= nick
-        }
-
-        message.scan(/^:([\\`\{\}\[\]\-_A-Z0-9\|\^]+)!.+?@.+? NICK :([\\`\{\}\[\]\-_A-Z0-9\|\^]+)/i){ |pnick, nick|
-            @names -= [pnick]
-            @names += [nick]
-        }
+        def include?(nick)
+            @names.include?(nick)
+        end
+    
+        def list
+            @names
+        end
     end
 
-    def include?(nick)
-        @names.include?(nick)
-    end
-
-    def list
-        @names
-    end
-end
-
-class KarmaBot
     def initialize(dbname, owner, nick, user, real, serv, port, chan, ssl = false)
         @arejoin = false
         @chan = chan
         @nick = nick
         @owner = owner
         @q = Queue.new
-        @u = Names.new(chan)
-        @k = Karma.new(dbname)
-        @s = Socket.new(serv, port, ssl)
+        @u = KarmaBot::Names.new(chan)
+        @k = KarmaBot::Karma.new(dbname)
+        @s = KarmaBot::Socket.new(serv, port, ssl)
         dispatcher
         append "USER #{user} 0 * :#{real}"
         append "NICK #{nick}"
@@ -378,7 +378,7 @@ end
 begin
     bot = KarmaBot.new(dbname, owner, nickname, username, realname, server, port, channel, ssl)
 rescue Exception => e
-    $stderr.puts e.to_s
+    $stderr.puts "Raised Exception: " + e.to_s
     exit
 end
 
